026ee98920ef9be14ad3a939e6920841dff96f3f
enrique <enrique@localhost>
2021-03-01 14:20:34 +0100

    Añadidos algunos comentarios

--- jpegCod.m
+++ jpegCod.m
@@ -1,11 +1,24 @@
+function imagenJPEG = jpegCod(x)
 %function imagenJPEG = jpegCod(x)
-%function imagenJPEG = jpegCod(x)
+%
+% Función de ejemplo para estudiar la aplicación de la codificación Huffman
+%a la codificación JPEG de imágenes. 
+%
+% Entrada:
+%  - x: imagen a codificar (para leer un archivo de imagen, utilizar
+%  imread). La función está pensada para imágenes en escala de grises. 
+%
+% Salida:
+%  - imagenJPEG: La imagen codificada, sin utilizar codificación entrópica.
 
 
+
+%Para poder trabajar más cómodo, convierto la imagen a formato 'double'.
 if ~isa(x,'double')
     x = double(x);
 end
 
+%Matriz de cuantificación JPEG
 Q = [ 16 11 10 16 24 40 51 61;
      12 12 14 19 26 58 60 55;
      14 13 16 24 40 57 69 56;
@@ -15,16 +28,17 @@ Q = [ 16 11 10 16 24 40 51 61;
      49 64 78 87 103 121 120 101;
      72 92 95 98 112 100 103 99];
  
+%Voy reservando espacio para la imagen codificada
 imagenJPEG = zeros(size(x));
 
 
-%Codificación de la imagen
+%Proceso de codificación por bloques 8x8
 for i=1:8:size(x,1)
     for j=1:8:size(x,2)
-        bloque = x(i:i+7, j:j+7);
-        DCTbloque = dct2(bloque);
-        bloqueQ = round(DCTbloque./Q);
-        imagenJPEG(i:i+7,j:j+7) = bloqueQ;
+        bloque = x(i:i+7, j:j+7);           % Cojo el bloque
+        DCTbloque = dct2(bloque);           % Calculo la DCT
+        bloqueQ = round(DCTbloque./Q);      % Cuantifico
+        imagenJPEG(i:i+7,j:j+7) = bloqueQ;  % Lo paso a la imagen global
     end
 end
 
--- jpegDec.m
+++ jpegDec.m
@@ -1,5 +1,17 @@
 function y = jpegDec(imagenJPEG)
+%function y = jpegDec(imagenJPEG)
+%
+% Función de ejemplo para estudiar la aplicación de la codificación Huffman
+%a la codificación JPEG de imágenes. 
+%
+% Entrada:
+%  - imagenJPG: imagen codificada en JPEG, tal cual sale de la función jpegCod 
+%
+% Salida:
+%  - y: La imagen reconstruida.
 
+
+%Matriz de cuantificación (es la misma que en el codificador
 Q = [ 16 11 10 16 24 40 51 61;
      12 12 14 19 26 58 60 55;
      14 13 16 24 40 57 69 56;
@@ -10,16 +22,20 @@ Q = [ 16 11 10 16 24 40 51 61;
      72 92 95 98 112 100 103 99];
  
 
-%Decodificación
+%Reservo espacio para la imagen de salida
 y = zeros(size(imagenJPEG));
+
+%Proceso de decodificación (bloques 8x8)
 for i=1:8:size(imagenJPEG,1)
     for j=1:8:size(imagenJPEG,2)
-        bloque = imagenJPEG(i:i+7, j:j+7);
-        bloqueIQ = bloque.*Q;
-        IDCTbloque = idct2(bloqueIQ);
-        bloqueR = IDCTbloque;
-        y(i:i+7,j:j+7) = bloqueR;
+        bloque = imagenJPEG(i:i+7, j:j+7);  %Cojo el bloque
+        bloqueIQ = bloque.*Q;               %Cuantificación inversa
+        IDCTbloque = idct2(bloqueIQ);       %IDCT  
+        y(i:i+7,j:j+7) = IDCTbloque;        %Lo paso a la imagen global
     end
 end
 
-y = uint8(y);
+%Por compatibilidad, vuelvo al formato uint8. 
+y = uint8(y);   
+
+%Para ver la imagen, imshow
